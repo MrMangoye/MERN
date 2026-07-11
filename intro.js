1d873693997bacbef3c5dc355c6ec0d5edde18cc
MrMangoye <benjaminjuma512@gmail.com>
2026-07-11 19:57:37 +0300

    repair

--- intro.js
+++ intro.js
@@ -1,11 +1,11 @@
 const name = "Benjamin Juma Mangoye";
 const age = 22;
-const favouritelanguage = "JavaScript";
+const favoritelanguage = "JavaScript";
 
 console.log("=".repeat(50));
 console.log("  PERSONAL INFORMATION");
 console.log("=".repeat(50));
 console.log("Name: " + name);
 console.log("Age: " + age);
-console.log("Favorite Programming Language:" + favouriteLanguge);
+console.log("Favorite Programming Language:" + favoriteLanguge);
 console.log("=".repeat(50));
